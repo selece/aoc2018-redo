a0df088dc898d7b6efdeae58e9df7b0249ed3c5e
Jonathan Liaw <jonathan.liaw@campbellsci.ca>
2019-03-06 14:00:35 -0700

    Day 3 part 2 solved, refactored Fabric.js

--- solutions/03.1.js
+++ solutions/03.1.js
@@ -1,28 +1,10 @@
 const { read } = require('../util/reader');
-const { Fabric } = require('../util/day03-Fabric');
+const { Fabric } = require('../util/Fabric');
 
 const processor = (data) => {
-  const processed = data
-    .split('\n')
-    .map((line) => {
-      const { groups: { id, x, y, w, h } } = line.match(Fabric.pattern);
-      return {
-        id,
-        x: Number.parseInt(x, 10),
-        y: Number.parseInt(y, 10),
-        w: Number.parseInt(w, 10),
-        h: Number.parseInt(h, 10),
-      };
-    });
-
+  const processed = data.split('\n');
   const fabric = new Fabric(processed.length);
-  processed.forEach(({
-    x: _x, y: _y, w: _w, h: _h,
-  }) => {
-    fabric.write({
-      _x, _y, _w, _h,
-    });
-  });
+  fabric.init(processed);
 
   return fabric.calc(2);
 };
