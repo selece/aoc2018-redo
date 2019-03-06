32a0916935a51f67e83bc3c5db9a49afdfbf8ec2
Jonathan Liaw <jonathan.liaw@campbellsci.ca>
2019-03-06 10:45:55 -0700

    Day 01-2 solved.

--- solutions/01.2.js
+++ solutions/01.2.js
@@ -1,6 +1,35 @@
 const { read } = require('../util/reader');
 
-const processor = data => data;
+function* loopedGeneratorFrom(arr) {
+  let idx = 0;
+  while (true) {
+    yield arr[idx % arr.length];
+    idx += 1;
+  }
+}
+
+const processor = (data) => {
+  const processed = data
+    .split('\n')
+    .map(e => Number.parseInt(e, 10));
+
+  const values = loopedGeneratorFrom(processed);
+  let frequency = 0;
+  let found;
+  const search = [];
+
+  while (found === undefined) {
+    frequency += values.next().value;
+
+    if (search.includes(frequency)) {
+      found = frequency;
+    } else {
+      search.push(frequency);
+    }
+  }
+
+  return found;
+};
 
 read('./inputs/day01.txt', processor).then((solution) => {
   console.log(solution);
