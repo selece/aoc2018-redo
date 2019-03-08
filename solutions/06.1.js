27eabe94995c2b50c20fede1553f68f32f8b38b4
Jonathan Liaw <jonathan.liaw@campbellsci.ca>
2019-03-08 13:19:26 -0700

    Added nearestMap and window functions to InfiniteGrid

--- solutions/06.1.js
+++ solutions/06.1.js
@@ -3,7 +3,7 @@ const { InfiniteGrid } = require('../util/InfiniteGrid');
 
 read('./inputs/day06-test.txt', (data) => {
   const grid = new InfiniteGrid(data.split('\n'));
-  return grid.data;
+  return grid.nearestMap(grid.window({ x: 4, y: 4 }, 4));
 }).then((solution) => {
   console.log(solution);
 });
--- util/InfiniteGrid.js
+++ util/InfiniteGrid.js
@@ -26,14 +26,68 @@ class InfiniteGrid {
     // mark edge points (infinite edges - exclude from interior)
     this.data = this.data
       .map(point => ({
-        point,
-        edge: (
-          point.x === this.extents.xmin
-          || point.x === this.extents.xmax
-          || point.y === this.extents.ymin
-          || point.y === this.extents.ymax
-        ),
+        ...point,
+        edge: this.isEdge(point),
       }));
+
+    // grab all interior points
+    this.inside = this.data
+      .filter(e => e.edge === false);
+  }
+
+  isEdge({ x, y }) {
+    return x === this.extents.xmin
+      || x === this.extents.xmax
+      || y === this.extents.ymin
+      || y === this.extents.ymax;
+  }
+
+  isInside({ x, y }) {
+    return x > this.extents.xmin
+      || x < this.extents.xmax
+      || y > this.extents.ymin
+      || y < this.extents.ymax;
+  }
+
+  window({ x, y }, size) {
+    const xmin = Math.max(x - size, this.extents.xmin);
+    const xmax = Math.min(x + size, this.extents.xmax);
+    const ymin = Math.max(y - size, this.extents.ymin);
+    const ymax = Math.min(y + size, this.extents.ymax);
+
+    const xrange = Array(Math.max(xmax - xmin + 1, 0)).fill(0).map((e, i) => e + i + xmin);
+    const yrange = Array(Math.max(ymax - ymin + 1, 0)).fill(0).map((e, i) => e + i + ymin);
+
+    return xrange.reduce((xres, i) => {
+      xres.push(
+        yrange.reduce((yres, j) => {
+          yres.push({ x: i, y: j });
+          return yres;
+        }, []),
+      );
+
+      return xres;
+    }, []).flat();
+  }
+
+  nearestMap(input) {
+    return input
+      .map(({ x, y }) => this.data
+        .map(e => ({
+          ...e,
+          distance: InfiniteGrid.distance({ x, y }, { ...e }),
+        }))
+        .reduce((min, elem) => {
+          if (elem.distance < min.distance) {
+            min = { ...elem }; // eslint-disable-line no-param-reassign
+          }
+
+          return min;
+        }, { distance: Infinity, id: undefined }));
+  }
+
+  static distance({ x: xa, y: ya }, { x: xb, y: yb }) {
+    return Math.abs(xa - xb) + Math.abs(ya - yb);
   }
 }
 
